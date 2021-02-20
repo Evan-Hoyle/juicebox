eccf9f945dc50870cdb82beb7c8efa36824c5a71
Evan-Hoyle <ehoyleconsulting@gmail.com>
2021-02-20 14:16:44 -0600

    Started part 3 writing create POST

--- DataBase/index.js
+++ DataBase/index.js
@@ -339,6 +339,27 @@ async function getUserByUsername(username) {
   }
 }
 
+async function createPost({
+  authorId,
+  title,
+  content,
+  tags = []
+}) {
+  try {
+    const { rows: [ post ] } = await client.query(`
+      INSERT INTO posts("authorId", title, content) 
+      VALUES($1, $2, $3)
+      RETURNING *;
+    `, [authorId, title, content]);
+
+    const tagList = await createTags(tags);
+
+    return await addTagsToPost(post.id, tagList);
+  } catch (error) {
+    throw error;
+  }
+}
+
 
 module.exports = {  
   client,
@@ -355,5 +376,6 @@ module.exports = {
   getAllTags,
   createPostTag,
   addTagsToPost,
-  getUserByUsername
+  getUserByUsername,
+  createPost
 }
