faabe2d37f9af0a7a39cd740464544b329345b59
Evan-Hoyle <ehoyleconsulting@gmail.com>
2021-02-20 13:30:27 -0600

    Added more from part 2

--- api/users.js
+++ api/users.js
@@ -1,6 +1,8 @@
+const { JWT_SECRET } = process.env;
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const usersRouter = express.Router();
-const { getAllUsers } = require('../DataBase');
+const { getAllUsers, getUserByUsername } = require('../DataBase');
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -14,6 +16,36 @@ usersRouter.get('/', async (req, res) => {
   res.send({
     users
   });
+});
+
+
+usersRouter.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      const token = jwt.sign(user, JWT_SECRET);
+      res.send({ message: "you're logged in!", token });
+    } else {
+      next({ 
+        name: 'IncorrectCredentialsError', 
+        message: 'Username or password is incorrect'
+      });
+    }
+  } catch(error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 module.exports = usersRouter;
